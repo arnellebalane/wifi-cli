31990d97dc996bbb714c60bb286bdc72ffc473a6
Arnelle Balane <arnellebalane@gmail.com>
2016-08-28 18:42:18 +0800

    add iface option to readme and help message

--- cli.js
+++ cli.js
@@ -1,14 +1,19 @@
 #!/usr/bin/env node
 const meow = require('meow');
+const wifi = require('wifi');
 
 
 const cli = meow(`
   usage:
 
-    wf                        - display current connection
-    wf list                   - list available networks
-    wf connect <id|ssid>      - connect to a networrk
-    wf disconnect             - disconnect from current network
+    wf                      - display current connection
+    wf list                 - list available networks
+    wf connect <id|ssid>    - connect to a networrk
+    wf disconnect           - disconnect from current network
+
+  options:
+
+    -i, --iface             - specify a wireless interface to use
 `);
 
 const command = cli.input[0] || 'list';
