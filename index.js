5cad8251bb3083a54b042d4035e8da57436f7a62
William <wlbberman@gmail.com>
2018-05-12 19:09:11 -0700

    updated to async await and put whole of index in a class

--- index.js
+++ index.js
@@ -1,116 +1,125 @@
 const shell = require('shelljs');
 const multilineToJsonArray = require('./lib/multiline-to-json-array');
 
-function execute(command) {
+class WF {
+
+  constructor() {
+    return new Proxy(this, {
+      get: (obj, prop) => {
+        if (prop in obj) {
+          const value = Reflect.get(obj, prop);
+          if (typeof value === 'function') {
+            return value.bind(obj);
+          } else {
+            return value;
+          }
+        } else {
+          return undefined;
+        }
+      }
+    });
+  }
+
+  execute(command) {
     return new Promise((resolve, reject) => {
-        shell.exec(command, {
-            silent: true
+      shell.exec(command, {
+        silent: true
         }, (code, stdout, stderr) => {
-            if (code) {
-                return reject(stderr.trim());
-            }
+          if (code) {
+            reject(stderr.trim());
+          } else {
             resolve(stdout.trim());
+          }
         });
     });
-}
+  }
 
 
-function status() {
-    return execute('nmcli d | grep wifi')
-        .then((network) => {
-            network = network.split(/\s{2,}/);
-            if (network[2] === 'connected') {
-                return {
-                    name: network[3]
-                };
-            }
-            return false;
-        });
-}
+  async status() {
+    let network = await this.execute('nmcli d | grep wifi');
+    network = network.split(/\s{2,}/);
+    if (network[2] === 'connected') {
+      return {
+        name: network[3].split(' ')[0]
+      };
+    }
+    return false;
+  }
 
 
-function scan() {
-    return execute('nmcli -m multiline device wifi list')
-        .then(multilineToJsonArray)
-        .then(networks => {
-            return networks.map(network => {
-                network.ssid = network.ssid.replace(/^'|'$/g, '');
-                return network;
-            });
-        })
-        .then(networks => networks.sort((a, b) => b.signal - a.signal));
-}
+  async scan() {
+    const networks = multilineToJsonArray(
+      await this.execute('nmcli -m multiline device wifi list'));
+    return networks.map(network => {
+      network.ssid = network.ssid.replace(/^'|'$/g, '');
+      return network;
+    }).sort((a, b) => b.signal - a.signal);
+  }
 
-function findNetwork(id, networks) {
+  findNetwork(id, networks) {
     if (typeof id === 'number' && id > 0 && id <= networks.length) {
         return networks[id - 1];
     }
     return networks.find(network => network.ssid === id);
-}
+  }
 
-function networkFromScan(id) {
-    return scan().then(networks => {
-        return findNetwork(id, networks);
-    });
-}
+  async networkFromScan(id) {
+    const networks = await this.scan();
+    return this.findNetwork(id, networks);
+  }
 
-function networkFromHistory(id) {
-    return history().then(networks => {
-        return findNetwork(id, networks);
-    });
-}
+  async networkFromHistory(id) {
+    const networks = await this.history();
+    return this.findNetwork(id, networks);
+  }
 
-function connect(ssid, password) {
+  async connect(ssid, password) {
     let command = `nmcli device wifi connect "${ssid}"`;
     if (password) {
         command += ` password "${password}"`;
     }
-    return execute(command).then(status);
-}
+    return await this.status(
+      await this.execute(command));
+  }
 
-
-function disconnect() {
-    return status().then(connection => {
-        if (connection) {
-            return execute(`nmcli connection down id "${connection.name}"`)
-                .then(output => connection);
-        }
-        return connection;
-    });
-}
-
-function history() {
-    return execute('nmcli -m multiline connection')
-        .then(multilineToJsonArray)
-        .then(network => {
-            return network.filter((network) => {
-                return network.type === '802-11-wireless';
-            });
-        })
-        .then(networks => networks.sort((a, b) => a.name == b.name ? 0 : a.name < b.name ? -1 : 1));
-}
-
-function forget(ssid) {
-    if (ssid) {
-        return networkFromHistory(ssid).then(network => {
-            return execute(`nmcli connection delete '${network.name}'`).then(status);
-        });
+  async disconnect() {
+    const connection = await this.status();
+    if (connection) {
+      const output = await this.execute(`nmcli connection down id "${connection.name}"`);
+      return connection;
+    } else {
+      return connection;
     }
+  }
 
-    return history().then(networks => {
-        if (networks.length === 0) {
-            throw new Error('You have no connection history');
-        }
-        const connectionList = networks.map((a) => `'${a.name}'`).join(" ");
-        return execute(`nmcli connection delete ${connectionList}`).then(status);
-    });
+  async history() {
+    const network = multilineToJsonArray(
+      await this.execute('nmcli -m multiline connection'));
+
+    return network.filter((network) => {
+      return network.type === '802-11-wireless';
+    }).sort((a, b) => a.name == b.name ? 0 : a.name < b.name ? -1 : 1);
+  }
+
+  async forget(ssid) {
+    if (ssid) {
+      const network = await this.networkFromHistory(ssid);
+      return await this.status(
+        this.execute(`nmcli connection delete '${network.name}'`));
+    } else {
+      const networks = this.history();
+
+      if (networks.length === 0) {
+        throw new Error('You have no connection history');
+      }
+
+      const connectionList =
+            networks.map((a) => `'${a.name}'`).join(" ");
+
+      return await this.status(
+        await this.execute(`nmcli connection delete ${connectionList}`));
+    }
+  }
 }
 
-exports.status = status;
-exports.scan = scan;
-exports.networkFromScan = networkFromScan;
-exports.networkFromHistory = networkFromHistory;
-exports.connect = connect;
-exports.disconnect = disconnect;
-exports.history = history;
-exports.forget = forget;
+module.exports = new WF();
